eb1385e9c85bba08bf115aec933215e700e3684a
kmnazrul <nazrulislam.1292@gmail.com>
2020-08-29 16:15:42 +0600

    updated final

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,10 +7,10 @@ const Footer = () => {
     return (
         <div className="socialLink">
             <h5>Follow us on</h5>
-            <a href="/fb">Facebook</a>
-            <a href="/twitter">Twitter</a> 
-            <a href="/instagram">Instagram</a>
-            <a href="/linkedIn">LinkedIn</a>
+            <a href="https://www.facebook.com/K.M.NAZRUL">Facebook</a>
+            <a href="https://twitter.com/k_nazrul">Twitter</a> 
+            <a href="https://www.instagram.com/k.m.nazrul/">Instagram</a>
+            <a href="https://www.linkedin.com/in/nazrul-islam-6525289b/">LinkedIn</a>
             
         </div>
     );
